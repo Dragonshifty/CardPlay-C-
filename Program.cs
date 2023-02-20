d21636aaa7393cef7676fc91bf48de7b7d511f17
James Fitt <91226933+Dragonshifty@users.noreply.github.com>
2023-02-20 16:15:33 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CardPlayConsole
+{
+    internal class Program
+    {
+        private static Card one;
+        private static Card two;
+        static void Main(string[] args)
+        {
+            
+            LinkedList<Card> deck = new LinkedList<Card>(Card.GetPack()); 
+            foreach (Card card in deck)
+            {
+                if (card.GetCardNumber() == 2)
+                {
+                    one = card;
+                } else if (card.GetCardNumber() == 4)
+                {
+                    two = card;
+                }
+                
+            }
+
+            Console.WriteLine($"{one} vs {two}");
+            Console.WriteLine(one.CompareTo(two));
+            Console.WriteLine(Card.CheckSuitMatch(one, two));
+
+
+            
+            
+        }
+    }
+}
